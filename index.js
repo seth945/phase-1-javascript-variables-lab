7e148d596e61796ced00c0c4f8672a2bab78aaaf
seth baraka <barakasetho5@gmail.com>
2025-03-23 07:45:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+describe('index.js', function () {
+    Swahili homes 
+});
+it('is set as Scuber', function () {
+    Swahili homes
+});
+it('is set as Scuber', function () {
+    expect(swahili Homes).to.equal('Scuber');
+  });
+  let swahili homes = 'Scuber';
